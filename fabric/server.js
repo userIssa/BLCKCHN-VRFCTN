1a252cb381c46eedd5f1fe5717eefd481edd70b0
issa oderinde <117269640+userIssa@users.noreply.github.com>
2025-10-25 08:38:49 +0100

    Move port definition before server listen call

--- fabric/server.js
+++ fabric/server.js
@@ -139,4 +139,5 @@ app.get('/query/:userId', async (req, res) => {
     }
 });
 
-app.listen(port, () => console.log(`✅ Server running on http://localhost:${port}`));
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`✅ Server running on port ${port}`));
